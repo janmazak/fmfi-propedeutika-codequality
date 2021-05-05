364cd7925896cb93463f6eea2962198ee4ec8151
Michaela Mickova <michaela.mickova111@gmail.com>
2021-05-06 00:00:00 +0200

    simplified matrix filling

--- ConcatenateMatrices.java
+++ ConcatenateMatrices.java
@@ -9,21 +9,21 @@ public class ConcatenateMatrices {
         int m = scanner.nextInt();
         int n = scanner.nextInt();
 
-        String[][] matrix = new String[m][n]; // vyroba matice
+        String[][] matrix = new String[m][n]; // vyroba prazdnej matice
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = "";
             }
         }
-        while (scanner.hasNextLine()) { //cita pokial nenarazi na koniec (neexistujuci prvy riadok matice)
-            for (int i = 0; i < m; i++) { //ak je dalsi riadok, tak urcite bude m-riadkov matice
-                String riadok = scanner.nextLine(); //jeden riadok matice
-                String[] prvky = riadok.split(" "); //rozdelenie riadku na prvky (stlpce)
-                for (int j = 0; j < n; j++) { //prvky_length = n
-                    matrix[i][j] += prvky[j]; //pricitanie do vyslednej matice
+
+        while (scanner.hasNext()) { // kym je cim, plni sa matica
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] += scanner.next();
                 }
             }
         }
+
         for (int i = 0; i < m; i++) { //formatovany vystup vyslednej matice
             for (int j = 0; j < n; j++) {
                 System.out.printf("[%d,%d]: %s\n", i, j, matrix[i][j]);
