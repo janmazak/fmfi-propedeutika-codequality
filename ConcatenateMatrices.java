40ca33b6d0e2ed81d8d476d9cbc051370cb608f1
Michaela Mickova <michaela.mickova111@gmail.com>
2021-05-06 12:34:24 +0200

    tests added

--- ConcatenateMatrices.java
+++ ConcatenateMatrices.java
@@ -10,6 +10,8 @@ public class ConcatenateMatrices {
         int m = scanner.nextInt();
         int n = scanner.nextInt();
 
+        System.out.println(areDimensionsSet(m, n));
+
         String[][] matrix = new String[m][n]; // vyroba prazdnej matice
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -25,6 +27,8 @@ public class ConcatenateMatrices {
             }
         }
 
+        System.out.println(isMatrixFilled(m, n, matrix));   
+
         for (int i = 0; i < m; i++) { //formatovany vystup vyslednej matice
             for (int j = 0; j < n; j++) {
                 output.printf("[%d,%d]: %s\n", i, j, matrix[i][j]);
@@ -32,5 +36,29 @@ public class ConcatenateMatrices {
         }
         output.close();
         scanner.close();
+    }
+
+    /**
+     * UNIT TESTS
+     */
+
+    public static boolean areDimensionsSet(Integer m, Integer n)
+    {
+        if (m > 0 && n > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isMatrixFilled(Integer m, Integer n, String[][] matrix)
+    {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == "") {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
