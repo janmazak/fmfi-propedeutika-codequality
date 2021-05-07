6a89e4fcc312157e2f284512def84583d31a00de
jitka1997 <jitka.mur@gmail.com>
2021-05-07 18:24:05 +0200

    separation of reading next matrix and adding it to the result using dedicated Matrix class methods, new methods in Matrix class for easier usage

--- ConcatenateMatrices.java
+++ ConcatenateMatrices.java
@@ -3,13 +3,6 @@ import java.io.*;
 
 public class ConcatenateMatrices {
 
-    private static String[][] makeResultMatrix(int m, int n){
-        String[][] resultMatrix = new String[m][n];
-        for (String[] strings : resultMatrix) {
-            Arrays.fill(strings, "");
-        }
-        return resultMatrix;
-    }
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
@@ -19,25 +12,36 @@ public class ConcatenateMatrices {
         String outputFileName = scanner.nextLine();
         Scanner input = new Scanner(new File(inputFileName));
 
+
         int m = input.nextInt(), n = input.nextInt();
         input.nextLine();
 
-        Matrix matrix = new Matrix(m,n);
+        Matrix matrix = new Matrix(m, n);
 
+        // TODO what to do with exceptions
+        while (input.hasNext()) {
+            Matrix nextMatrix = null;
+            // reading next matrix from input file
+            try {
+                nextMatrix = Matrix.readMatrix(input, m, n);
+            } catch (NoSuchElementException e) {
+                System.out.println("Nesprávne zadaná matica");
+                break;
+            }
 
-        while (input.hasNextLine()) { //cita pokial nenarazi na koniec (neexistujuci prvy riadok matice)
-            for (int i = 0; i < m; i++) { //ak je dalsi riadok, tak urcite bude m-riadkov matice
-                String row = input.nextLine(); //jeden riadok matice
-                String[] columns = row.split(" "); //rozdelenie riadku na prvky (stlpce)
-                for (int j = 0; j < n; j++) { //prvky_length = n
-                    matrix.addTo(i, j, columns[j]); //pricitanie do vyslednej matice
-                }
+            // adding successfully read matrix to the result matrix
+            try {
+                matrix.addMatrix(nextMatrix);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Matice nie sú rovnakého typu");
+                break;
             }
         }
+
         PrintStream output = new PrintStream(outputFileName);
         for (int i = 0; i < m; i++) { //formatovany vystup vyslednej matice
             for (int j = 0; j < n; j++) {
-                output.printf("[%d,%d]: %s\n", i, j, matrix.get(i,j));
+                output.printf("[%d,%d]: %s\n", i, j, matrix.get(i, j));
             }
         }
         input.close();
--- Matrix.java
+++ Matrix.java
@@ -3,34 +3,68 @@ import java.io.*;
 
 public class Matrix {
     private String[][] matrix;
+    private int numOfRows;
+    private int numOfColumns;
 
     public Matrix(int m, int n) {
+        numOfRows = m;
+        numOfColumns = n;
         matrix = new String[m][n];
         for (String[] line : matrix) {
             Arrays.fill(line, "");
         }
     }
 
-    // try to read matrix of m rows and n columns, not used, not tested yet, TODO: custom exceptions
-    public void readMatrix(Scanner scanner, int m, int n) {
+    // try to read matrix of m rows and n columns TODO: custom exceptions
+    public static Matrix readMatrix(Scanner scanner, int m, int n) {
+        Matrix matrix = new Matrix(m, n);
         for (int i = 0; i < m; i++) {
             String row = null;
             if (scanner.hasNextLine()) {
                 row = scanner.nextLine();
-            }else throw new NoSuchElementException();
+            } else throw new NoSuchElementException();
             String[] columns = row.split(" ");
-            if(columns.length != n) throw new NoSuchElementException();
-            for(int j =0; j < m; j++){
-                matrix[i][j] = columns[j];
+            if (columns.length != n) throw new NoSuchElementException();
+            for (int j = 0; j < n; j++) {
+                matrix.set(i, j, columns[j]);
+            }
+        }
+        return matrix;
+    }
+
+    public void addTo(int row, int col, String value) {
+        matrix[row][col] += value;
+    }
+
+    public void addMatrix(Matrix matrix) {
+        if (numOfRows != matrix.numOfRows || numOfColumns != matrix.numOfColumns) {
+            throw new IllegalArgumentException();
+        }
+        for (int row = 0; row < numOfRows; row++) {
+            for (int col = 0; col < numOfColumns; col++) {
+                this.addTo(row, col, matrix.get(row, col));
             }
         }
     }
 
-    public void addTo(int row, int col, String value){
-        matrix[row][col] += value;
+    public String get(int row, int col) {
+        return matrix[row][col];
     }
 
-    public String get(int row, int col){
-        return matrix[row][col];
+    public void set(int row, int col, String value) {
+        matrix[row][col] = value;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (String[] row : matrix) {
+            for (String col : row) {
+                result += col;
+                result += " ";
+            }
+            result += "\n";
+        }
+        return result;
     }
 }
