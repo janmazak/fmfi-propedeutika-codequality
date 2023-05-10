80a999a38c003dfd7c5d10512499f1a0325ba1ff
Ondrej Škorňák <130708513+OndrejSkornak@users.noreply.github.com>
2023-05-10 22:13:46 +0200

    Make changes to the variables. Make changes to improve code readability.

--- ConcatenateMatrices.java
+++ ConcatenateMatrices.java
@@ -4,40 +4,45 @@ import java.io.*;
 public class ConcatenateMatrices {
 
     public static void main(String[] args) throws IOException {
-
+        //checking if input file exists
         File inputFile = new File("vstup1.txt");
         if (!inputFile.exists()) {
             System.err.println("Input file does not exist");
             return;
         }
 
+        //reading input file
         Scanner scanner = new Scanner(inputFile);
         PrintStream output = new PrintStream("vystup.txt");
 
+        //reading dimensions of matrix
         String dimensionsLine = scanner.nextLine();
         String[] dimensions = dimensionsLine.split(" ");
         int numRows = Integer.parseInt(dimensions[0]);
         int numCols = Integer.parseInt(dimensions[1]);
 
+        //initialization of matrix
         String[][] matrix = new String[numRows][numCols];
         for (String[] row : matrix) {
             Arrays.fill(row, "");
         }
-
-        while (scanner.hasNextLine()) { //cita pokial nenarazi na koniec (neexistujuci prvy riadok matice)
-            for (int i = 0; i < numRows; i++) { //ak je dalsi riadok, tak urcite bude m-riadkov matice
-                String riadok = scanner.nextLine(); //jeden riadok matice
-                String[] prvky = riadok.split(" "); //rozdelenie riadku na prvky (stlpce)
-                for (int j = 0; j < numCols; j++) { //prvky_length = n
-                    matrix[i][j] += prvky[j]; //pricitanie do vyslednej matice
+        //reading values from input file and concatenating them to matrix
+        while (scanner.hasNextLine()) {
+            for (int row = 0; row < numRows; row++) {
+                String[] rowValues = scanner.nextLine().split(" ");
+                for (int col = 0; col < numCols; col++) {
+                    matrix[row][col] += rowValues[col];
                 }
             }
         }
-        for (int i = 0; i < numRows; i++) { //formatovany vystup vyslednej matice
+        //printing matrix to output file
+        for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
                 output.printf("[%d,%d]: %s\n", i, j, matrix[i][j]);
             }
         }
+
+        output.close();
         scanner.close();
     }
 }
